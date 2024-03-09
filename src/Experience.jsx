a28be11c00d34edf1930e898d70d05d6d3e9d5ab
Kevin Velez <kevin.alejandro.velez@correounivalle.edu.co>
2024-03-09 14:58:12 -0500

    Creación de los cubos con diferentes materiales mesh. Hay error en el mesh MeshDistanceMaterial

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,6 +1,13 @@
 import { OrbitControls } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
+import { MeshLambertMaterial } from "three";
+import { MeshDistanceMaterial } from "three";
+import { meshMatcapMaterial } from "three";
+import { meshNormalMaterial } from "three";
+import { meshPhongMaterial, } from "three";
+import { meshPhysicalMaterial } from "three";
+import { meshToonMaterial } from "three";
 
 const Floor = () => {
     return (
@@ -10,12 +17,99 @@ const Floor = () => {
       </mesh>
     );
   };
-const Experience = () => {
-    const boxRef = useRef(null);
-    
-    useFrame((state, delta)=>{
-        boxRef.current.rotation.x += 1 * delta;
+  
+  const MyMesh = ({ position, color }) => {
+    return (
+      <mesh position={position}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshLambertMaterial color={color} />
+      </mesh>
+    );
+  };
+
+  const MyMesh2 = ({ position, color }) => {
+    return (
+      <mesh position={position}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshDistanceMaterial color={color} />
+      </mesh>
+    );
+  };
+
+  const MyMesh3 = ({ position, color }) => {
+    return (
+      <mesh position={position}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshMatcapMaterial color={color} />
+      </mesh>
+    );
+  };
+
+  const MyMesh4 = ({ position, color }) => {
+    return (
+      <mesh position={position}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshNormalMaterial color={color} />
+      </mesh>
+    );
+  };
+
+  const MyMesh5 = ({ position, color }) => {
+    return (
+      <mesh position={position}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshPhongMaterial color={color} />
+      </mesh>
+    );
+  };
+
+  const MyMesh6 = ({ position, color }) => {
+    return (
+      <mesh position={position}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshPhysicalMaterial color={color} />
+      </mesh>
+    );
+  };
+
+  const MyMesh7 = ({ position, color }) => {
+    return (
+      <mesh position={position}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshToonMaterial color={color} />
+      </mesh>
+    );
+  };
+
+  /*const MeshWithMaterial = ({ position, color }) => {
+    const ref = useRef();
+  
+    useFrame((state, delta) => {
+      ref.current.rotation.x += 1 * delta;
     });
+  
+    return (
+      <mesh ref={ref} position={position}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color={color} />
+      </mesh>
+    );
+  };
+  <MeshWithDistanceMaterial position={[-2, 1, 0]} color="green" />
+            <MeshWithMaterial position={[-1, 1, 0]} color="blue" />
+            <MeshWithMaterial position={[0, 1, 0]} color="red" />
+            <MeshWithMaterial position={[1, 1, 0]} color="yellow" />
+            <MeshWithMaterial position={[2, 1, 0]} color="purple" />
+            <MeshWithMaterial position={[3, 1, 0]} color="orange" />
+            <MeshWithMaterial position={[4, 1, 0]} color="cyan" />
+  */
+  
+const Experience = () => {
+    //const boxRef = useRef(null);
+    
+    //useFrame((state, delta)=>{
+        //boxRef.current.rotation.x += 1 * delta;
+    //});
 
     return (
         <>
@@ -23,10 +117,13 @@ const Experience = () => {
             <directionalLight position={[10, 20, 5]} />
             <OrbitControls makeDefault />
             <Floor />
-            <mesh ref={boxRef} position={[0, 3, 0]}>
-                <boxGeometry args={[1, 1, 1]} />
-                <meshStandardMaterial color={"purple"} />
-            </mesh>
+            <MyMesh position={[0, 5, 0]} color="red" />
+            <MyMesh position={[3, 5, 0]} color="green" />
+            <MyMesh3 position={[6, 5, 0]} color="blue" />
+            <MyMesh4 position={[9, 5, 0]} color="yellow" />
+            <MyMesh5 position={[12, 5, 0]} color="purple" />
+            <MyMesh6 position={[15, 5, 0]} color="orange" />
+            <MyMesh7 position={[18, 5, 0]} color="cyan" />
         </>
 
     )
