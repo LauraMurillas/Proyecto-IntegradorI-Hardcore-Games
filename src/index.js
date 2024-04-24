f583009179c7e06e37e8dc76f433de80c2c73b1c
Kevin Velez <kevin.alejandro.velez@correounivalle.edu.co>
2024-04-24 11:13:24 -0500

    upload changes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
+import Experience from "./Experience";
 import "./styles.css";
 import { createRoot } from "react-dom/client";
-import Experience from "./Experience";
-import { Canvas } from "@react-three/fiber";
 
 const root = createRoot(document.getElementById("root"));
 
-root.render(
-    <Canvas
-        camera={
-            {
-                position: [0, 10, 20]
-            }
-        }
-    >
-        <Experience />
-    </Canvas>
-
-);
+root.render(<Experience />);
