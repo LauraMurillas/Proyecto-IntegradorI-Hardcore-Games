d7d87b4a3951cbe8350d5ad5e02b4bc0a462f19d
Moreno <106407303+MMorenoMM19999@users.noreply.github.com>
2024-04-26 23:41:22 -0500

    nivel 2
    se implementa el nivel tres sin el dragon por ahora por que toca cambiar la escabilidad

--- src/routes/RoutesSquidGames.jsx
+++ src/routes/RoutesSquidGames.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Level1 from "../pages/level1/Level1";
+import Level2 from "../pages/level2/Level2";
 import Login from "../pages/login/Login";
 
 export default function RoutesSquidGames() {
@@ -8,6 +9,7 @@ export default function RoutesSquidGames() {
             <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/level1" element={<Level1 />} />
+                <Route path="/level2" element={<Level2 />} />
             </Routes>
         </BrowserRouter>
     )
