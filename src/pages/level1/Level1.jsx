b83dc43adaea5cd19bce2b17630becc87404413f
Kevin Velez <kevin.alejandro.velez@correounivalle.edu.co>
2024-05-10 20:52:05 -0500

    upload loader changes

--- src/pages/level1/Level1.jsx
+++ src/pages/level1/Level1.jsx
@@ -1,5 +1,5 @@
 import { Perf } from "r3f-perf";
-import { KeyboardControls, OrbitControls, Sparkles } from "@react-three/drei";
+import { KeyboardControls, Loader, OrbitControls, Sparkles } from "@react-three/drei";
 import { Physics } from "@react-three/rapier";
 import { Suspense, useState, useEffect, useRef } from "react";
 import WelcomeText from "./abstractions/WelcomeText";
@@ -91,6 +91,9 @@ export default function Level1() {
                     </Suspense>
 
                 </Canvas>
+                <Loader >
+                    { 'Cargando Nivel 1' }
+                </Loader>
             </KeyboardControls>
 
             {/* Control de volumen */}
